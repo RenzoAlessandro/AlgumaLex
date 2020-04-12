2b39817903eed117f14ae7e6cca5f05a15a59461
celson custodio <celsoncustodio@outlook.com>
2020-04-12 13:13:34 -0300

    Utilizando buffer

--- src/algumalex/LeitorDeArquivosTexto.java
+++ src/algumalex/LeitorDeArquivosTexto.java
@@ -12,22 +12,75 @@ import java.io.InputStream;
  * @author celson
  */
 public class LeitorDeArquivosTexto {
+    
+    private final static int TAMANHO_BUFFER = 5;
+    int[] bufferDeLeitura;
+    int ponteiro;
+    
     InputStream is;
     public LeitorDeArquivosTexto(String arquivo) {
         try {
             is = new FileInputStream(new File(arquivo));
+            inicializarBuffer();
         } catch (Exception ex) {
             ex.printStackTrace(System.err);
         }
     }
-    public int lerProximoCaractere() {
+   
+    private void inicializarBuffer() {
+        bufferDeLeitura = new int[TAMANHO_BUFFER * 2];
+        ponteiro = 0;
+        recarregarBuffer1();
+    }
+    
+    private void incrementarPonteiro() {
+        ponteiro++;
+        if (ponteiro == TAMANHO_BUFFER) {
+            recarregarBuffer2();
+        } else if (ponteiro == TAMANHO_BUFFER * 2) {
+            recarregarBuffer1();
+            ponteiro = 0;
+        }
+    }
+        private void recarregarBuffer1() {
         try {
-            int ret = is.read();
-            System.out.print((char)ret);
-            return ret;
+            for (int i = 0; i < TAMANHO_BUFFER; i++) {
+                bufferDeLeitura[i] = is.read();
+                if (bufferDeLeitura[i] == -1) {
+                    break;
+                }
+            }
         } catch (Exception ex) {
             ex.printStackTrace(System.err);
-            return -1;
+        }
+    }
+    private void recarregarBuffer2() {
+        try {
+            for (int i = TAMANHO_BUFFER; i < TAMANHO_BUFFER * 2; i++) {
+                bufferDeLeitura[i] = is.read();
+                if (bufferDeLeitura[i] == -1) {
+                    break;
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace(System.err);
+        }
+    }
+    private int lerCaractereDoBuffer() {
+        int ret = bufferDeLeitura[ponteiro];
+        incrementarPonteiro();
+        return ret;
+    }
+    
+     public int lerProximoCaractere() {
+        int c = lerCaractereDoBuffer();
+        System.out.print((char)c);
+        return c;
+    }
+    public void retroceder() {
+        ponteiro--;
+        if (ponteiro < 0) {
+            ponteiro = TAMANHO_BUFFER * 2 - 1;
         }
     }
 }
