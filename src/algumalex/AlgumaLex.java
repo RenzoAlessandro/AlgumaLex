e7700210638ec2d894b21a5693269ddee15dec93
celson custodio <celsoncustodio@outlook.com>
2020-04-12 13:56:54 -0300

    .

--- src/algumalex/AlgumaLex.java
+++ src/algumalex/AlgumaLex.java
@@ -19,7 +19,7 @@ public class AlgumaLex {
         AlgumaLexico lex = new AlgumaLexico(args[0]);
         Token t = null;
         while((t=lex.proximoToken()).nome != TipoToken.Fim) {
-            System.out.println(t);
+            System.out.print(t);
         }
     }
     
